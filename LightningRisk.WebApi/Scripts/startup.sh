659821924bff416c6a751b3f8fdce52e8a4dcd6d
Qin Guan <qinguan20040914@gmail.com>
2024-12-24 18:02:55 +0800

    Lol

--- LightningRisk.WebApi/Scripts/startup.sh
+++ LightningRisk.WebApi/Scripts/startup.sh
@@ -2,12 +2,13 @@
 
 if [ -f "/app/WTelegramClient.session" ]; then
   echo "Session file exists, skipping..."
-  cat "/app/WTelegramClient.session"
 else
   if [ -f "/app/WTelegramClient.session.base64" ]; then
     base64 -d "/app/WTelegramClient.session.base64" > /app/WTelegramClient.session;
+    echo "Decoded session file"
   else
     echo "Base64 encoded session file does not exist"
   fi
 fi
-    
+
+dotnet /app/LightningRisk.WebApi.dll
